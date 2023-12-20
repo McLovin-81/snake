86eae36a070b329910ff90b295c3f305bdfe602a
Cristian Felipe <estilo-callejero@hotmail.de>
2023-12-20 19:02:09 +0100

    works

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@ const int height = 20;
 const int width = 20;
 
 int x, y, fruitX, fruitY, score;
+
+int tailX[100], tailY[100], nTail;
+
 enum Direction {STOP, UP, DOWN, RIGHT, LEFT};
 Direction direction;
 
@@ -76,7 +79,20 @@ void draw()
             }
             else
             {
-                printw(" ");
+                bool print = false;
+
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (i == tailY[k] && j == tailX[k])
+                    {
+                        printw("o");
+                        print = true;
+                    }
+                }
+                if (!print)
+                {
+                    printw(" ");
+                }
             }
         }
         printw("\n");
@@ -87,6 +103,8 @@ void draw()
         printw("#");
     }
     printw("\n");
+
+    std::cout << "Score: " << score;
 }
 
 void input()
@@ -114,6 +132,22 @@ void input()
 
 void logic()
 {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    tailX[0] = x;
+    tailY[0] = y;
+    int prev2X, prev2Y;
+
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
     switch (direction)
     {
         case UP:
@@ -129,5 +163,26 @@ void logic()
             x--;
         default:
             break;
+    }
+
+    if (x < 0 || x > width || y < 0 || y > height)
+    {
+        gameOver = true;
+    }
+
+    for (int i = 0; i < nTail; i++)
+    {
+        if (x == tailX[i] && y == tailY[i])
+        {
+            gameOver = true;
+        }
+    }
+
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
     }
 }
